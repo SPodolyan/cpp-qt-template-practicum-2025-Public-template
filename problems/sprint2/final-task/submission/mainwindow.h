2068680e67089b9ea47c090d7d327045d279e647
Sergey Podolyan <spodolyan@mail.ru>
2025-11-05 19:51:50 +0000

    Revert "Sprint2-FinalTask-001"
    This reverts commit c27397927b1cddb01b4f9ded195485ca23a69b34.

--- problems/sprint2/final-task/submission/mainwindow.h
+++ problems/sprint2/final-task/submission/mainwindow.h
@@ -3,22 +3,12 @@
 #include "calculator.h"
 
 #include <QMainWindow>
-#include <QString>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
 QT_END_NAMESPACE
-
-enum class Operation {
-    NO_OPERATION,
-    ADDITION,
-    SUBTRACTION,
-    MULTIPLICATION,
-    DIVISION,
-    POWER
-};
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
@@ -28,30 +18,7 @@ public:
     ~MainWindow();
 
 private slots:
-    void OnDigitClicked();
-    void OnDotClicked();
-    void OnPlusMinusClicked();
-    void OnBackspaceClicked();
-    void OnOperationClicked();
-    void OnEqualsClicked();
-    void OnClearClicked();
-    void OnMemorySaveClicked();
-    void OnMemoryClearClicked();
-    void OnMemoryRecallClicked();
 
 private:
-    void SetText(const QString& text);
-    void AddText(const QString& suffix);
-    void SetOperation(Operation op);
-    QString OpToString(Operation op) const;
-    QString RemoveTrailingZeroes(const QString &text) const;
-    QString NormalizeNumber(const QString &text) const;
-
     Ui::MainWindow* ui;
-    Calculator calculator_;
-    QString input_number_;
-    Number active_number_;
-    Operation current_operation_;
-    double memory_value_;
-    bool has_memory_;
 };
